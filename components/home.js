b400f77e8359c59409e7fe1c3eb15577cebcd951
ninions15 <ninopaul.cervantes@ustp.edu.ph>
2022-10-13 22:09:42 +0800

    added styles

--- components/home.js
+++ components/home.js
@@ -1,27 +1,133 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, ImageBackground, StyleSheet, StatusBar, TextInput, TouchableOpacity, FlatList } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-native';
+import data from '../assets/data.json';
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  header: {
+    padding: 15,
+    paddingTop: StatusBar.currentHeight,
+    paddingBottom: 0,
+    flexDirection: 'row',
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1,
+  },
+  title: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  indicatorStyle: {
+    backgroundColor: '#6548B8',
+    height: 5,
+    borderLeftWidth: 70,
+    borderRightWidth: 70,
+    borderColor: 'white'
+  },
+  input: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#EFF0F7',
+    borderRadius: 15,
+    fontSize: 16
+  },
+})
+
+const TicketStyles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    padding: 15,
+    paddingHorizontal: 30,
+  },
+  imageBackground: {
+    width: '100%',
+    height: 150,
+    borderRadius: 10,
+    overflow: 'hidden'
+  },
+  info: {
+    padding: 15,
+    flexDirection: 'row',
     flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.3)'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  description: {
+    fontSize: 16,
+    color: 'white'
+  },
+  button: {
+    height: 45,
+    width: 45,
+    backgroundColor: 'white',
+    borderRadius: 45,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'yellow'
   }
 })
 
+const TicketItem = ({ item }) => {
+  return (
+    <View style={TicketStyles.container}>
+      <ImageBackground
+        source={{ uri: item.photo }}
+        borderRadius={10}
+        style={TicketStyles.imageBackground}
+      >
+        <View style={TicketStyles.info}>
+          <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+            <Text style={TicketStyles.title}>{item.title}</Text>
+            <Text style={TicketStyles.description}>P{item.price} • {item.date}</Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+
 const Home = () => {
   const navigate = useNavigate();
+  const [search, setSearch] = useState('');
+  const [tickets, setTickets] = useState([]);
+  
+  const onLogout = () => {
+    navigate('/')
+  };
+
+  useEffect(() => {
+    setTickets(data);
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Button
-        title='Login'
-        onPress={() => {
-          navigate('/')
-        }}
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <View style={{ flex: 1 }}>
+          <TextInput
+            style={styles.input}
+            value={search}
+            onChangeText={setSearch}
+            placeholder='Search Ticket'
+            underlineColorAndroid='transparent'
+          />
+        </View>
+        <View style={{ alignSelf: 'center', marginLeft: 15 }}>
+          <TouchableOpacity onPress={onLogout}>
+            <Text style={styles.title}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <FlatList
+        data={tickets}
+        renderItem={({ item }) => <TicketItem item={item} />}
+        ListHeaderComponent={() => <View style={{ height: 15 }} />}
       />
     </View>
   )
--- components/login.js
+++ components/login.js
@@ -1,28 +1,113 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
-import React from 'react'
-import { useNavigate } from 'react-router-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-native'
+
+const logo = require('../assets/TICKETSENPAI.png');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#412FB1',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  form: {
+    backgroundColor: 'white',
+    padding: 30,
+    borderRadius: 15,
+    width: '90%',
+    borderColor: '#e2e2e2',
+    borderWidth: 1,
+  },
+  input: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#EFF0F7',
+    borderRadius: 15,
+    fontSize: 16
+  },
+  button: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#412FB1',
     alignItems: 'center',
-    backgroundColor: 'red'
+    marginBottom: 10
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  buttonOutline: {
+    padding: 15,
+    borderRadius: 10,
+    borderColor: '#412FB1',
+    borderWidth: 1,
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  buttonOutlineText: {
+    color: '#412FB1',
+    fontSize: 16,
+  },
+  buttonDisabled: {
+    backgroundColor: 'rgba(65,47,177,0.5)',
+  },
+  image: {
+    marginBottom: 30,
+    width: 250,
+    height: 50
   }
 })
 
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [enabled, setEnabled] = useState(false);
+
+  const onLogin = () => {
+    navigate('/home');
+  }
+
+  useEffect(() => {
+    if (email && password) {
+      setEnabled(true);
+    } else {
+      setEnabled(false);
+    }
+  }, [email, password]);
 
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
-      <Button
-        title='Home'
-        onPress={() => {
-          navigate('/home')
-        }}
-      />
+      <Image style={styles.image} source={logo} resizeMode={'contain'} />
+      <View style={styles.form}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          placeholder='Email Address'
+          underlineColorAndroid='transparent'
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          placeholder='Password'
+          secureTextEntry={true}
+          underlineColorAndroid={'transparent'}
+        />
+        <TouchableOpacity disabled={!enabled} onPress={onLogin}>
+          <View style={[styles.button, !enabled && styles.buttonDisabled]}>
+            <Text style={styles.buttonText}>Login</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
